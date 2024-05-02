37e350fbe073cc0e5002d570e6b43d7baf4d5fc3
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2024-05-02 12:26:37 +0300

    Added basic session logic

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root "home#index"
 
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:new, :create, :destroy]
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
