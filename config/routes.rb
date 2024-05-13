b7662422ea984106657c64c85d955bd517535b74
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2024-05-13 20:49:11 +0300

    Initialized Order resource

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   resources :user_sessions, only: [:new, :create, :destroy]
 
+  resources :orders, only: [:index]
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 courier_user = User.create!(name: 'Александр', email: 'courier@email.com', password: 'qwerty', password_confirmation: 'qwerty', role: :courier)
 customer_user = User.create!(name: 'Владимир', email: 'customer@email.com', password: 'qwerty', password_confirmation: 'qwerty', role: :customer)
 admin_user = User.create!(name: 'Дмитрий', email: 'admin@email.com', password: 'qwerty', password_confirmation: 'qwerty', role: :admin)
+
+product = Product.create!(name: 'iPhone 15', price: 65_000, weight: 0.5)
+order_in_delivery = Order.create!(shipping_address: 'Марьино', delivery_address: 'Братиславская', status: 1, courier: courier_user, customer: customer_user)
+
+OrderProduct.create!(order: order_in_delivery, product: product)
