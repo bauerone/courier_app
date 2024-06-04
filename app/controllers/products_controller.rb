bbcf501e115a878b7bc819016e960cbb13d16bdf
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2024-06-05 01:07:38 +0300

    Added search to products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,6 @@ class ProductsController < ApplicationController
   before_action :stop_if_logged_out
 
   def index
-
     if params[:search].present?
       key = "%#{params[:search]}%"
       @products = Product.where("name LIKE ?", key)
