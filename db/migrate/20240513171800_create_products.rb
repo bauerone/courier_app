4451bd7ee06a5b6fb10e35a7a2e731ca03328297
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2024-06-04 18:43:39 +0300

    Added products list UI

--- db/migrate/20240513171800_create_products.rb
+++ db/migrate/20240513171800_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration[7.1]
   def change
     create_table :products do |t|
       t.string :name, null: false
+      t.string :description, null: false
       t.integer :price, null: false
       t.float :weight, null: false
       t.references :storage, null: false, foreign_key: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ User.create!(name: 'Дмитрий', email: 'admin@email.com', password: 'qwerty
 storage_bauman = Storage.create!(name: 'Склад на Бауманской', address: 'Бауманская')
 storage_frunze = Storage.create!(name: 'Склад на Фрунзенской', address: 'Фрунзенская')
 
-product_iphone = Product.create!(name: 'iPhone 15', price: 65_000, weight: 0.5, storage: storage_bauman)
-product_ipad = Product.create!(name: 'iPad Pro 13-inch', price: 120_000, weight: 0.6, storage: storage_frunze)
+product_iphone = Product.create!(name: 'iPhone 15', description: "Современное сбаланисрованное мобильное устройство. Черный цвет", price: 65_000, weight: 0.5, storage: storage_bauman)
+product_ipad = Product.create!(name: 'iPad Pro 13-inch', description: "Один из наиболее популярных устройств планшетного форм-фактора. Серебристый цвет.", price: 120_000, weight: 0.6, storage: storage_frunze)
 
 order_in_delivery = Order.create!(delivery_address: 'Братиславская', status: 1, courier: courier_user, customer: customer_user)
 
