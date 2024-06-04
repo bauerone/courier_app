27df10d1e69a4b5a0db6bbfa19a3bd9ce49b71fa
Dmitriy Malakhov <d.malakhov.v@gmail.com>
2024-06-04 17:12:57 +0300

    Fixed turbo events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @customers_count = User.customer.count
+    @couriers_count = User.courier.count
+    @products_count = Product.count
+    @orders_count = Order.count
   end
 end
